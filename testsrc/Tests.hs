2f211f03255001d9f5e656923be995ae11826aa4
John Goerzen <jgoerzen@complete.org>
2006-12-07 23:48:16 +0100

    Removed non-FTP tests

--- testsrc/Tests.hs
+++ testsrc/Tests.hs
@@ -18,46 +18,11 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 module Tests(tests) where
 import Test.HUnit
-import qualified MIMETypestest
-import qualified Listtest
-import qualified Maptest
-import qualified Pathtest
-import qualified Strtest
-import qualified IOtest
-import qualified Bitstest
 import qualified Network.FTP.Parsertest
-import qualified Eithertest
-import qualified CRC32POSIXtest
-import qualified CRC32GZIPtest
-import qualified GZiptest
-import qualified HVIOtest
-import qualified HVFStest
-import qualified Timetest
-import qualified Str.CSVtest
-import qualified WildMatchtest
-import qualified Globtest
-import qualified ProgressTrackertest
 
 test1 = TestCase ("x" @=? "x")
 
 tests = TestList [TestLabel "test1" test1,
-                 TestLabel "List" Listtest.tests,
-                 TestLabel "Str" Strtest.tests,
-                 TestLabel "CSV" Str.CSVtest.tests,
-                 TestLabel "Time" Timetest.tests,
-                 TestLabel "Map" Maptest.tests,
-                 TestLabel "ProgressTracker" ProgressTrackertest.tests,
-                 TestLabel "Path" Pathtest.tests,
-                 TestLabel "WildMatch" WildMatchtest.tests,
-                 TestLabel "HVIO" HVIOtest.tests,
-                 TestLabel "HVFS" HVFStest.tests,
-                 TestLabel "Glob" Globtest.tests,
-                 TestLabel "MIMETypes" MIMETypestest.tests,
-                 TestLabel "Bitstest" Bitstest.tests,
-                 TestLabel "Network.FTP.Parser" Network.FTP.Parsertest.tests,
-                 TestLabel "Eithertest" Eithertest.tests,
-                 TestLabel "CRC32POSIXtest" CRC32POSIXtest.tests,
-                 TestLabel "CRC32GZIPtest" CRC32GZIPtest.tests,
-                 TestLabel "GZiptest" GZiptest.tests]
+                 TestLabel "Network.FTP.Parser" Network.FTP.Parsertest.tests]
 
 
