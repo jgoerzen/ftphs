0bb525c7dfb2ce5fb9df84ca6ce9760ad24bf804
John Goerzen <jgoerzen@complete.org>
2009-04-06 10:18:48 -0500

    Fixed remaining test build issues

--- testsrc/Network/FTP/Parsertest.hs
+++ testsrc/Network/FTP/Parsertest.hs
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 module Network.FTP.Parsertest(tests) where
 import Test.HUnit
 import Network.FTP.Client.Parser
-import Test.HUnit.Utils
+import Test.HUnit.Tools
 import Network.Socket
 
 test_parseReply =
